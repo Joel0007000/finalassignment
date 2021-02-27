ca60190523013e72bb19a6ca33b632fcd0cc9b0d
Joel0007000 <58798161+Joel0007000@users.noreply.github.com>
2021-02-27 20:11:32 +0545

    DAO and Repository final update

--- app/src/main/java/com/joel/finalassignment/SignUpActivity.kt
+++ app/src/main/java/com/joel/finalassignment/SignUpActivity.kt
@@ -37,7 +37,11 @@ class SignUpActivity : AppCompatActivity() {
         btn_Have_an_account = findViewById(R.id.btn_Have_an_account)
         btn_SignUpGO = findViewById(R.id.btn_SignUpGO)
 
-
+        fullname = findViewById(R.id.fullname)
+        username = findViewById(R.id.username)
+        email = findViewById(R.id.email)
+        phone = findViewById(R.id.phone)
+        password = findViewById(R.id.password)
 
 
 
